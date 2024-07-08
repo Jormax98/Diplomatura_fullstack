d94b824ab8e7ff83b13923142ec01ca052530b3e
Maxi Romero <jorgemaxiromero@gmail.com>
2024-07-08 11:15:09 -0300

    U4_U3

--- M4_U3/app.js
+++ M4_U3/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const serviciosRouter = require('./routes/servicios');
+const contactoRouter = require('./routes/contacto');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/servicios', serviciosRouter);
+app.use('/contacto', contactoRouter);
+
+app.get('/home', function(req, res,next){
+
+  res.send('Página de inicio')
+
+})
+
+app.get('/trabajos', function(req, res,next){
+
+  res.send('Página de trabajos')
+
+})
+
+app.get('/empresa', function(req, res,next){
+
+  res.send('Página de empresa')
+
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
