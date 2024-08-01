4b9598bb1d806f17fa050723c39be91fa8242327
Maxi Romero <jorgemaxiromero@gmail.com>
2024-07-31 23:48:08 -0300

    M5_U4

--- jmdjs/back/app.js
+++ jmdjs/back/app.js
@@ -4,11 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var session = require('express-session');
+
 var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -23,6 +27,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
+  secret: 'b3C4(Js;',
+  cookie: { maxAge: null },
+  resave: false,
+  saveUninitialized: true
+}));
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.use(session({
   secret: 'ZJ4n5Xh6n9PC7m',
   resave: false,
   saveUninitialized: true
@@ -30,7 +54,8 @@ app.use(session({
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin/login', loginRouter); 
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // app.get('/', function (req, res) {
 //   var conocido = Boolean(req.session.nombre);
